f8abbbd259dbe20801e4640eb1cda726b02c91d1
Tarjei Olsen <tarjei.olsen@epinova.no>
2019-06-07 14:08:13 +0200

    Updated documentation

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Epinova's take on Arvato's AfterPay payemnt gateway API
 ![Tests](https://img.shields.io/appveyor/tests/Epinova_AppVeyor_Team/epinova-arvatopaymentgateway.svg)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-## Usage
+## Getting Started
+
 ### Add registry to Structuremap
 
 ```
@@ -26,3 +27,35 @@ Epinova's take on Arvato's AfterPay payemnt gateway API
 ### Inject contract and use service
 
 Epinova.ArvatoPaymentGateway.IInvoiceGatewayService describes the service. 
+
+### Prerequisites
+
+* [EPiServer.Framework](http://www.episerver.com/web-content-management) >= v11.1 for logging purposes.
+* [Automapper](https://github.com/AutoMapper/AutoMapper) >= v8.1 for mapping models.
+* [StructureMap](http://structuremap.github.io/) >= v4.7 for registering service contract.
+
+### Installing
+
+The module is published on nuget.org.
+
+```
+nuget install Epinova.ArvatoPaymentGateway
+```
+
+## Built With
+
+* .Net Framework 4.6.2
+
+## Authors
+
+* **Tarjei Olsen** - *Initial work* - [apeneve](https://github.com/apeneve)
+
+See also the list of [contributors](https://github.com/Epinova/Epinova.ArvatoPaymentGateway/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Further reading
+
+[AfterPay API documentation](https://developer.afterpay.io/api)
--- src/IInvoiceGatewayService.cs
+++ src/IInvoiceGatewayService.cs
@@ -5,12 +5,35 @@ namespace Epinova.ArvatoPaymentGateway
 {
     public interface IInvoiceGatewayService
     {
+        /// <summary>
+        /// Approves the payment for a specified customer and basket. Main use-cases are One-Step and Two-Step Authorization.
+        /// Full fraud and credit scoring applied.
+        /// </summary>
         Task<AuthorizeResponse> AuthorizeAsync(string authorizationKey, AuthorizeRequest request);
+        /// <summary>
+        /// Returns the available installment plans for the specific request amount.
+        /// Returns monthly installment amount, interest and fees.
+        /// </summary>
         Task<AvailableInstallmentPlansResponse> AvailableInstallmentPlansAsync(string authorizationKey, AvailableInstallmentPlansRequest request);
+        /// <summary>
+        /// Cancel (Void) an authorization that has not been captured.
+        /// </summary>
         Task<CancelResponse> CancelAsync(string authorizationKey, string orderNumber);
+        /// <summary>
+        /// Completes the payment that has been authorized. Typically done when the order is shipped. Can be a full or partial capture of the order amount.
+        /// </summary>
         Task<CaptureResponse> CaptureAsync(string authorizationKey, CaptureRequest request);
+        /// <summary>
+        /// Completes the payment that has been authorized in full. Typically done when the order is shipped.
+        /// </summary>
         Task<CaptureResponse> CaptureFullAsync(string authorizationKey, string orderNumber);
+        /// <summary>
+        /// Refunds a partially or fully captured payment.
+        /// </summary>
         Task<CreditResponse> CreditAsync(string authorizationKey, CreditRequest request);
+        /// <summary>
+        /// Refunds a captured payment in full.
+        /// </summary>
         Task<CreditResponse> CreditFullAsync(string authorizationKey, string orderNumber);
         Task<OrderResponse> GetOrderAsync(string authorizationKey, string orderNumber);
         Task<Version> GetVersionAsync(string authorizationKey);
