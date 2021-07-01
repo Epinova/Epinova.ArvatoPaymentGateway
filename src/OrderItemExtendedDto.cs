55b63ac36d5020e425c97afcb851d7b31ed38629
Hans Christian Thjømøe <hct@epinova.no>
2021-07-01 13:23:12 +0200

    Add image and status fields

--- src/OrderItemExtendedDto.cs
+++ src/OrderItemExtendedDto.cs
@@ -5,6 +5,7 @@ namespace Epinova.ArvatoPaymentGateway
 {
     internal class OrderItemExtendedDto : BaseOrderItemDto
     {
+        public string ImageUrl { get; set; }
         public DateTime InsertedAt { get; set; }
         public Guid OrderId { get; set; }
         public DateTime UpdatedAt { get; set; }
